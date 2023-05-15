e736c72f37099a769252dc15675c7eb67b15a33f
Quadas <musadaq23@hotmail.com>
2023-05-15 07:00:40 -0400

    added helpers file

---
+++ src/components/Pages/Resume.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Resume() {
+    return (
+        <div className="resume">
+            <h2>Resume</h2>
+
+            <p>coming soon<a href="https://www.google.ca/">resume</a></p>
+
+            <h3>
+                Front-end skills
+            </h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>responsive design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul>
+            <h3>
+                Back-end skills
+            </h3>
+            <ul>
+                <li>API</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+            </ul>
+        </div>
+    )
+}
